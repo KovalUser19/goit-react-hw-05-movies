a6bed32743b7a1288a2a061f82539d3a1e4782e0
KovalUser19 <134967853+KovalUser19@users.noreply.github.com>
2023-12-30 19:54:15 +0000

    Deploying to gh-pages from @ KovalUser19/goit-react-hw-05-movies@717a9de6be59b32a1a13a909d280ecbd49f9b8a3 🚀

--- static/js/490.690fe896.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[490],{490:function(e,n,t){t.r(n);var r=t(791),o=t(184),a=(0,r.lazy)((function(){return"../components/Home/Home.jsx"}));n.default=function(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("h1",{children:"Trending today"}),(0,o.jsx)(a,{})]})}}}]);
-//# sourceMappingURL=490.690fe896.chunk.js.map
